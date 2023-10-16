b34fceb0cf5d155151f67bba7b8a024db5b89794
Arion2000 <github@arion2000.xyz>
2023-10-16 13:40:30 +0200

    Update ping.ts

--- api/ping.ts
+++ api/ping.ts
@@ -1,8 +1,10 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
+  const startTime = process.hrtime();
+  const endTime = process.hrtime(startTime);
+  const durationInMilliseconds = endTime[0] * 1000 + endTime[1] / 1e6;
   return res.json({
-    message: `Hello ${name}!`,
+    message: `Die Seite wurde in ${durationInMilliseconds} ms geladen.`,
   })
 }
