0697a7a77407d2267083943bb026571a4706447d
Arion2000 <github@arion2000.xyz>
2023-10-16 13:57:25 +0200

    Update ping.ts

--- api/ping.ts
+++ api/ping.ts
@@ -5,7 +5,7 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
   const startTime = process.hrtime();
   const endTime = process.hrtime(startTime);
   const durationInMilliseconds = endTime[0] * 1000 + endTime[1] / 1e6;
-  if (lang == 'de') {
+  if (lang == 'de' || lang != 'en') {
     return res.status(200).send(`Die Seite wurde in ${durationInMilliseconds}ms geladen.`)
   } else if (lang == 'en') {
     return res.status(200).send(`The page was loaded in ${durationInMilliseconds}ms.`)
