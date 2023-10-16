2d08a87ec96e7f4dadb6c1980ea48c6a814abfeb
Arion2000 <github@arion2000.xyz>
2023-10-16 13:20:51 +0200

    First create

--- index.ts
+++ index.ts
@@ -1,8 +1,31 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import fetch from 'node-fetch';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
-  })
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  try {
+    // API-URL für Discord-Guild-Widget
+    const { guildId = '831161440705839124' } = req.query
+    const apiUrl = `https://discord.com/api/guilds/${guildId}/widget.json`;
+
+    // Die API abrufen
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(`Fehler beim Abrufen der API: ${response.status} ${response.statusText}`);
+    }
+
+    const data: any = await response.json(); // Hier wird "any" als Datentyp verwendet
+
+    const instantInvite = data.instant_invite;
+
+
+    // Weiterleitung zur Discord-Einladung
+    res.writeHead(302, { Location: instantInvite });
+    res.end();
+    return res.json({
+        url: `${instantInvite}`,
+      })
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Ein Fehler ist aufgetreten');
+  }
 }
