b74d9e80f92814c3db8d748dcd2c551cf5a54703
Arion2000 <github@arion2000.xyz>
2023-10-16 13:55:43 +0200

    Added english texts

--- api/ping.ts
+++ api/ping.ts
@@ -1,10 +1,13 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  const { lang = 'de' } = req.query;
   const startTime = process.hrtime();
   const endTime = process.hrtime(startTime);
   const durationInMilliseconds = endTime[0] * 1000 + endTime[1] / 1e6;
-  return res.json({
-    message: `Die Seite wurde in ${durationInMilliseconds} ms geladen.`,
-  })
+  if (lang == 'de') {
+    return res.status(200).send(`Die Seite wurde in ${durationInMilliseconds}ms geladen.`)
+  } else if (lang == 'en') {
+    return res.status(200).send(`The page was loaded in ${durationInMilliseconds}ms.`)
+  }
 }
