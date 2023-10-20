3cf0acc89a47fa726fd93bbbc34cf67409b9f85f
Arion2000 <github@arion2000.xyz>
2023-10-20 06:41:58 +0200

    Update ping.ts

--- api/ping.ts
+++ api/ping.ts
@@ -2,12 +2,10 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { lang = 'de' } = req.query;
-  const startTime = process.hrtime();
-  const endTime = process.hrtime(startTime);
-  const durationInMilliseconds = endTime[0] * 1000 + endTime[1] / 1e6;
+  const loadingTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
   if (lang == 'de' || lang != 'en') {
-    return res.status(200).send(`Die Seite wurde in ${durationInMilliseconds}ms geladen.`)
+    return res.status(200).send(`Die Seite wurde in ${loadingTime}ms geladen.`)
   } else if (lang == 'en') {
-    return res.status(200).send(`The page was loaded in ${durationInMilliseconds}ms.`)
+    return res.status(200).send(`The page was loaded in ${loadingTime}ms.`)
   }
 }
