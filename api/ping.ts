35bf492e2400f9eff842207222ab80e677db6fb4
Arion2000 <github@arion2000.xyz>
2023-10-20 06:45:01 +0200

    Update ping.ts

--- api/ping.ts
+++ api/ping.ts
@@ -2,7 +2,14 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { lang = 'de' } = req.query;
-  const loadingTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
+  const start = Date.now();
+
+  // Simuliere eine asynchrone Aufgabe (z.B. eine Datenbankabfrage oder eine API-Anfrage)
+  // Du kannst diese Aufgabe durch eine echte Aufgabe ersetzen.
+  await new Promise(resolve => setTimeout(resolve, 100));
+
+  const end = Date.now();
+  const loadingTime = end - start;
   if (lang == 'de' || lang != 'en') {
     return res.status(200).send(`Die Seite wurde in ${loadingTime}ms geladen.`)
   } else if (lang == 'en') {
