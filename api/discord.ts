9b8ee4c5e8bcb68990a905715e6c8af750c3b144
Arion2000 <github@arion2000.xyz>
2024-04-01 17:02:26 +0200

    Update discord.ts

--- api/discord.ts
+++ api/discord.ts
@@ -2,21 +2,18 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
-    // API-URL für Discord-Guild-Widget
     const { guildId = '831161440705839124' } = req.query;
     const apiUrl = `https://discord.com/api/guilds/${guildId}/widget.json`;
 
-    // Die API abrufen
     const response = await fetch(apiUrl);
     if (!response.ok) {
       throw new Error(`${response.status} ${response.statusText}`);
     }
 
-    const data: any = await response.json(); // Hier wird "any" als Datentyp verwendet
+    const data: any = await response.json();
 
     const instantInvite = data.instant_invite;
 
-    // Weiterleitung zur Discord-Einladung
     res.writeHead(302, { Location: instantInvite });
     res.end();
   } catch (error) {
